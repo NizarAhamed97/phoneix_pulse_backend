ddc3c3f9b566fbfccf790dd9139df78b140e0982
NizarAhamed97 <nizarahamedm33@gmail.com>
2025-04-30 16:55:58 +0530

    password

--- src/database/dbcon.ts
+++ src/database/dbcon.ts
@@ -4,9 +4,8 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', // Replace with your MySQL username
-  password: '', // Replace with your MySQL password
+  password: 'password', // Replace with your MySQL password
   database: 'daily_fit', // Replace with your database name
-  port : 3306
 });
 
 // Connect to the MySQL database
